b4742d2789f5eade0f82c137e094f7c18e242dea
Jack Collinson <phantekx@gmail.com>
2020-02-29 23:56:54 +1300

    unpack.extract_vehicle_info working, get_vehicles working apart from edge cases such as no .json

--- unpack.py
+++ unpack.py
@@ -4,7 +4,7 @@ import json
 import csv
 import subprocess
 from shutil import copy, rmtree
-
+import re
 import sys
 
 # Class to store driver attributes
@@ -34,6 +34,17 @@ def represents_int(li):
 		except ValueError:
 			return False
 	return True
+
+# Cleans given directory (loop necessary to catch odd PermissionErrors [WinError 5])
+def create_dir(dir):
+	i = 0
+	while i < 100:
+		i += 1
+		try:
+			os.makedirs(dir)
+		except OSError:
+			continue
+	return None	
 
 # Read in settings from json
 def read_json():
@@ -105,7 +116,26 @@ def unpack_mas(vehicle, vehicle_dir, temp_dir, mod_mgr_path, mas = "car-upgrade.
 
 # Extracts vehicle info from vehicle_contents
 def extract_vehicle_info(vehicle_contents, tag):
-	pass
+	#print(f"tage = {tag}")
+	#print(f"vehicle = contents = {vehicle_contents}")
+	# tag = "Team=.*\""
+	#(Team=[^"]+")$
+	new_tag = tag.split(".")[0]
+	new_tag = "(" + new_tag + "\"[^\"]+\")$"
+	#print(f"new_tag = {new_tag}")
+	p = re.compile(new_tag)
+	#print(p)
+	for string in vehicle_contents:
+		match = p.search(string)
+		if match:
+			q = "(\"[^!@&*]+\")$"
+			q = re.compile(q)
+			result = q.search(match[0])[0]
+			result = result.replace("\"","")
+			if result == None:
+				print("Error extracting vehicle info.")
+				sys.exit()
+	return result
 
 # Extracts vehicles
 def get_vehicles(vehicle_dir, temp_dir, output_dir, vehicles, mod_mgr_path):
@@ -113,14 +143,14 @@ def get_vehicles(vehicle_dir, temp_dir, output_dir, vehicles, mod_mgr_path):
 	# Create empty temp folder
 	if os.path.exists(Path(temp_dir)):
 		rmtree(temp_dir)
-		os.makedirs(temp_dir)
+		create_dir(temp_dir)
 	else:
-		os.makedirs(temp_dir)
+		create_dir(temp_dir)
 
 	vehicle_inventory = {}
 	for vehicle in vehicles:
 		# Unpack vehicle
-		unpack_mas(vehicle, vehicle_dir, temp_dir, mod_mgr_path, mas = "car_upgrade.mas")
+		unpack_mas(vehicle, vehicle_dir, temp_dir, mod_mgr_path, mas = "car-upgrade.mas")
 
 		# Get all vehicle files and catalog all extracted files
 		all_files = os.listdir(temp_dir)
@@ -128,48 +158,54 @@ def get_vehicles(vehicle_dir, temp_dir, output_dir, vehicles, mod_mgr_path):
 		veh_files = [x for x in all_files if x.count(".veh") != 0]
 
 		# Loop over vehicle files to extract info
-		veh_files_dicts = {}
+		veh_files_dicts = dict()
 		for idx, veh in enumerate(veh_files):
 			try:
-				veh_dict = {}
-				with open(temp_dir + veh) as file:
-					veh_contents = readLines(files)
+				veh_dict = dict()
+				with open(temp_dir + "/" + veh) as file:
+					veh_contents = file.readlines()
 					veh_dict["idx"] = idx
 					veh_dict["dds_file"] = extract_vehicle_info(veh_contents, "DefaultLivery=.*dds\"")
 					veh_dict["team"] = extract_vehicle_info(veh_contents, "Team=.*\"")
 					veh_dict["full_team_name"] = extract_vehicle_info(veh_contents, "FullTeamName=.*\"")
-					veh_base = veh.replace(".dds","")
+					veh_base = veh_dict["dds_file"][:-4]
 					veh_dict["dds_region_file"] = veh_base + "_region.dds"
 					veh_dict["json_file"] = veh_base + ".json"
 					veh_dict["veh_folder"] = veh_base
 
-			except:
-				print("Error in reading .veh files. If you see this error something has probably gone seriously wrong.")
+			except Exception as err:
+				print(err)
+				if err == PermissionError:
+					print("Permission error in creating directory")
+				else:
+					print("Error in reading .veh files. If you see this error something has probably gone seriously wrong.")
 				sys.exit()
-			veh_files_dicts[idx] = veh_dict
+			veh_files_dicts.update({str(idx): veh_dict})
 
 		# Clean directory before file copy
-		if os.path.exists(output_dir):
-			rmtree(output_dir)
-			os.makedirs(output_dir)
+		if not os.path.exists(output_dir):
+			create_dir(output_dir)
 
 		# File copy
-		copy_dir = Path(output_dir, vehicle, veh_files_dicts["veh_folder"])
-		os.makedirs(copy_dir)
-		shutil.copy(Path(temp_dir, veh), copy_dir)
-		shutil.copy(Path(temp_dir, veh_dict["dds_file"]), copy_dir)
-		shutil.copy(Path(temp_dir, veh_dict["dds_region_file"]), copy_dir)
-		shutil.copy(Path(temp_dir, veh_dict["json_file"]), copy_dir)
+		for idx, veh_dict in enumerate(veh_files_dicts.values()):
+			#print(type(veh_dict))
+			#print(f"veh_dict = {veh_dict}")
+			copy_dir = Path(output_dir, vehicle, veh_files[idx])
+			create_dir(copy_dir)
+			copy(Path(temp_dir, veh), copy_dir)
+			copy(Path(temp_dir, veh_dict["dds_file"]), copy_dir)
+			copy(Path(temp_dir, veh_dict["dds_region_file"]), copy_dir)
+			copy(Path(temp_dir, veh_dict["json_file"]), copy_dir)
 
 		# Write veh_files_dict to vehicle_inventory
 		vehicle_inventory[vehicle] = veh_files_dicts
 
 	# Delete temp directory
-	if os.exists(temp_dir):
-		shutil.rmtree(temp_dir)
+	if os.path.exists(temp_dir):
+		rmtree(temp_dir)
 	
 	# Write vehicle inventory
-	with open("vehicle_inventory.json", "-w", encoding="uft-8") as file:
+	with open("vehicle_inventory.json", "w") as file:
 		json.dump(vehicle_inventory, file, ensure_ascii=False, indent=4)
 
 	return None
